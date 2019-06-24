f9cc6719469d2707ad9c9bd560187f15bc25200e
unknown <s.e.maddahi.ir@gmail.com>
2019-06-24 21:11:49 +0430

    chack username exists and get user with specific api_key

--- app/Base/Config/UserDatabase.php
+++ app/Base/Config/UserDatabase.php
@@ -4,65 +4,70 @@ namespace Base\Config;
 class UserDatabase{
 
     private $connection ;
+
+    // users table name
     private $table_name = 'users';
 
     // set connection value
-    function __construct($connection){
+    public function __construct($connection){
         $this->connection = $connection;
     }
 
-    // basic 
-    function add_user(array $user_information){
+    public function add_user(array $user_information){
         // get basic informations 
         $name_family = $user_information['name_family'];
         $email       = $user_information['email'];
         $username    = $user_information['username'];
         $salt        = uniqid();
-        $password    = create_secure_password($user_information['password'], $salt);
-        $api_key     = create_hash_value(uniqid());
+        $password    = $this->create_secure_password( $user_information['password'], $salt);
+        $api_key     = $this->create_hash_value(uniqid());
         $score       = 0;
         
-        // add user SQL query
-        $query = "INSERT INTO users (name_family, email, username, salt, password, api_key, score) 
-                    Values(?, ?, ?, ?, ?, ?, ?)";
-        
+        // add user in users database
+        $query = "INSERT INTO {$this->table_name} (name_family, email, username, salt, password, api_key, score) VALUES
+                    ('{$name_family}', '{$email}', '{$username}', '{$salt}', '{$password}', '{$api_key}', {$score})";
 
-        $data=[
-            'name_family' => $name_family,
-            'email'       => $email, 
-            'username'    => $username, 
-            'salt'        => $salt, 
-            'password'    => $password, 
-            'api_key'     => $api_key,
-            'score'       => $score
-        ];
-
-
-        // add user 
-        $this->connection->prepare($query)->execute($data);
-
-        return $api_key;
+        // execute query
+        if ($this->connection->query($query)) {
+            return "done";
+        }else{
+            return "error";
+        }
     }
     
-    function get_user($api_key){
-        $query     = "SELECT * FROM {$this->table_name}";
-        $statement = $this->connection->prepare($query);
-        $statement -> execute();
-        return $statement; 
+    public function get_user($api_key){
+        $sql = "SELECT * FROM users Where api_key='$api_key'";
+        $result = $this->connection->query($sql);
+        if($result->num_rows > 0){
+            return $result->fetch_assoc();
+        }else{
+            return "api_key isn't correct";
+        } 
+    }
+
+    // check username is already exists or not
+    public function is_exists_username($username){
+        $sql = "SELECT * FROM {$this->table_name} WHERE username='$username'";
+        $result = $this->connection->query($sql);
+        if($result->num_rows > 0){
+            return "username already exists";
+        }else{
+            return null;
+        }
     }
 
 
     // produce secure password
-    function create_secure_password($password, $salt){
+    public function create_secure_password($password, $salt){
         // mix password and salt
-        $pass_plus_salt = $password+$salt;
+        $pass_plus_salt = $password."".$salt;
         
-        $hashed_password = create_hash_value($pass_plus_salt);
+        $hashed_password = $this->create_hash_value($pass_plus_salt);
         return $hashed_password;
     }
 
 
-    function create_hash_value($value){
+    public function create_hash_value($value){
         return hash('sha256', $value);
     }
 
