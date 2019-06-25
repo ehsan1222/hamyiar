833e07fd2f7ae7010838c1334f857acbe754a024
unknown <s.e.maddahi.ir@gmail.com>
2019-06-25 23:20:35 +0430

    uodate autoload class

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Base\\Config\\CompanyDatabase' => $baseDir . '/app/Base/Config/CompanyDatabase.php',
     'Base\\Config\\Database' => $baseDir . '/app/Base/Config/Database.php',
+    'Base\\Config\\ProjectDatabase' => $baseDir . '/app/Base/Config/ProjectDatabase.php',
     'Base\\Config\\UserDatabase' => $baseDir . '/app/Base/Config/UserDatabase.php',
     'Base\\Middleware\\UserMiddleware' => $baseDir . '/app/Base/Middleware/UserMiddleware.php',
     'Core\\Routing' => $baseDir . '/app/Core/Routing.php',
